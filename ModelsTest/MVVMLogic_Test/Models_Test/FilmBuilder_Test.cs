d06d586be4cbee34c8eb56f51e6e49266477ea57
Grigoriy Paravyan <gvalal@bk.ru>
2022-01-03 20:08:11 +0300

    Пишу unit тест для проверки скроллера фоновых картинок

--- ModelsTest/MVVMLogic_Test/Models_Test/FilmBuilder_Test.cs
+++ ModelsTest/MVVMLogic_Test/Models_Test/FilmBuilder_Test.cs
@@ -9,16 +9,48 @@ namespace ModelsTest.MVVMLogic_Test.Models_Test
     [TestClass]
     public class FilmBuilder_Test
     {
+        private List<Film> _films = new List<Film>();
+
+        [TestMethod]
+        private async Task CreatingFilms_Test()
+        {
+            FilmParser filmBuilder = new FilmParser();
+
+            filmBuilder.OnFilm += film => _films.Add(film);
+            await filmBuilder.CreatingFilms();
+
+            System.Diagnostics.Stopwatch myStopwatch = new System.Diagnostics.Stopwatch();
+            myStopwatch.Start(); 
+            
+            int currentCount = 0;
+            while (_films.Count != 5)
+            {
+                
+                if (currentCount < _films.Count)
+                {
+                    currentCount = _films.Count;
+                    myStopwatch.Restart();
+                }
+                else if (myStopwatch.ElapsedMilliseconds > 5000)
+                    break;
+            }
+            myStopwatch.Stop();
+        }
+
         [TestMethod]
         public async Task CreatingFilms_are_equal_count()
         {
-            List<Film> films = new List<Film>();
-            FilmParser filmBuilder = new FilmParser();
+            await CreatingFilms_Test();
 
-            filmBuilder.OnFilm += film => films.Add(film);
-            await filmBuilder.CreatingFilms();
+            Assert.AreEqual(5, _films.Count);
+        }
 
-            Assert.AreEqual(5, films.Count);
+        [TestMethod]
+        public async Task CreatingFilms_all_items_notnull()
+        {
+            await CreatingFilms_Test();
+
+            CollectionAssert.AllItemsAreNotNull(_films);
         }
     }
 }
---
+++ ModelsTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Films.Classes.BingSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Films.MVVMLogic.Models.ImagesScroll;
+
+namespace ModelsTest
+{
+    [TestClass]
+    public class ImagesTest
+    {
+        [TestMethod]
+        public void Images_Download()
+        {
+            FilmBuilder images = new FilmBuilder();
+
+            images.CreatingFilms();
+        }
+    }
+}
