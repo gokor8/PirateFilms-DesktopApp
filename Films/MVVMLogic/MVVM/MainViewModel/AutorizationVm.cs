807d86d64623f016997e8f52570f34ad44573d70
Grigoriy Paravyan <gvalal@bk.ru>
2022-01-13 14:32:57 +0300

    Перемещаю Loginnerов в отдельную папку. Добавляю AutorizationVm отдельную асинхронную задачу с токеном отмены

--- Films/MVVMLogic/MVVM/MainViewModel/AutorizationVm.cs
+++ Films/MVVMLogic/MVVM/MainViewModel/AutorizationVm.cs
@@ -1,12 +1,16 @@
-﻿using Films.Classes.MVVM.Buttons;
+﻿using System.Threading.Tasks;
+using Films.Classes.MVVM.Buttons;
 using System.Windows.Controls;
 using System.Windows.Input;
 using Films.MVVMLogic.Models;
+using System.Threading;
 
 namespace Films.MVVMLogic.MVVM
 {
     internal class AutorizationVm
     {
+        private CancellationTokenSource _singUpToken = new CancellationTokenSource();
+
         private string login;
         public string Login
         {
@@ -17,11 +21,12 @@ namespace Films.MVVMLogic.MVVM
             }
         }
 
-        public ICommand SignUp => new DelegateCommand(obj =>
-                {
-                    // напсиать юнит тест
-                    var loginnerDataStore = new Loginner().VerifyLogin(Login).VerifyPassword(GetPassword(obj)).GetVerifyResult();
-                    if (loginnerDataStore.IsVerify)
+        public ICommand SignUp => new DelegateCommand(async obj =>
+                { 
+                    var loginnerTask = await Task.Run(() =>
+                        new Loginner().VerifyLogin(Login).VerifyPassword(GetPassword(obj)).GetVerifyResult(), _singUpToken.Token);
+                    
+                    if (loginnerTask.IsVerify)
                     {
                         //В основное окно идем епте
                         System.Windows.MessageBox.Show("Logged");
@@ -37,7 +42,8 @@ namespace Films.MVVMLogic.MVVM
 
         public ICommand SignByGhost => new DelegateCommand((obj) =>
                {
-                   // Залетаем в новое окно
+                   _singUpToken.Cancel();
+
                    System.Windows.MessageBox.Show("CALAM2");
                });
 
