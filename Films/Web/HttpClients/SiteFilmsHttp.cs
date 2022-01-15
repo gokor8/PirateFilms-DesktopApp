9223d812db615f275b8df707af88b22c99d85f3d
Grigoriy Paravyan <gvalal@bk.ru>
2022-01-15 19:57:05 +0300

    Меняю ссылку на сайт

--- Films/Web/HttpClients/SiteFilmsHttp.cs
+++ Films/Web/HttpClients/SiteFilmsHttp.cs
@@ -13,10 +13,10 @@ namespace Films.Web.HttpClients
         }
         private void SetSiteHeaders()
         {
-            Client.DefaultRequestHeaders.Host = "w.lordfilm.cfd";
+            //Client.DefaultRequestHeaders.Host = "w.lordfilm.cfd";
             Client.DefaultRequestHeaders.Add("Referer", "https://yandex.ru/");
             Client.DefaultRequestHeaders.Add("User-Agent", "Fiddler Everywhere");
-            Client.BaseAddress = new Uri("https://w.lordfilm.cfd/film/");
+            Client.BaseAddress = new Uri("https://lordz.lordfilm.cfd/film/");
         }
 
         public override Task<Stream> GetStreamClient(string link)
