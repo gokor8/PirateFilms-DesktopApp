063e212604fba4d277ab199977ae6dfb36a79f71
Grigoriy Paravyan <gvalal@bk.ru>
2022-01-20 15:45:10 +0300

    Исправляю ошибку с подключением к бд

--- Films/Models/DataBaseLogic/User.cs
+++ Films/Models/DataBaseLogic/User.cs
@@ -1,7 +1,11 @@
-﻿namespace Films.MVVMLogic.Models.DataBaseLogic
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Films.MVVMLogic.Models.DataBaseLogic
 {
     public class User
     {
+        [Key]
+        public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
     }
