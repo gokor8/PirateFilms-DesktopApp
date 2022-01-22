91ac79b2fef098eb8f00a7751d96c9cd2332d352
Grigoriy Paravyan <gvalal@bk.ru>
2022-01-22 20:50:08 +0300

    Изменяю вид BindablePasswordBox

---
+++ Films/Views/Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Films.Views.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для BindablePasswordBox.xaml
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        public static readonly DependencyProperty PropertyTypeProperty = DependencyProperty.Register(
+            "Password", typeof(string),
+            typeof(BindablePasswordBox), new FrameworkPropertyMetadata(
+                string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, PasswordPropertyChanged));
+
+        private static void PasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is BindablePasswordBox passwordBox)
+            {
+                passwordBox.UpdatePassword();
+            }
+        }
+
+        private bool _isPasswordChanging;
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        public string Password
+        {
+            get
+            {
+                string val = (string)GetValue(PropertyTypeProperty);
+                return (string)GetValue(PropertyTypeProperty);
+            }
+            set
+            {
+                SetValue(PropertyTypeProperty, value);
+            }
+        }
+
+        private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            _isPasswordChanging = true;
+            Password = PasswordBox.Password;
+            PasswordTextBox.Text = PasswordBox.Password;
+            _isPasswordChanging = false;
+        }
+
+        private void UpdatePassword()
+        {
+            if (!_isPasswordChanging)
+            {
+                PasswordBox.Password = Password;
+            }
+        }
+    }
+}
