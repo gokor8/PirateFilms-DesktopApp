1d3ff27dfdd63a0d86cf9ed201367fe143b364cd
Grigoriy Paravyan <gvalal@bk.ru>
2022-01-31 22:15:26 +0300

    Меняю бингу заголовок User-Agent на распозноваемый

--- Films/Models/Web/HttpClients/BaseHttp.cs
+++ Films/Models/Web/HttpClients/BaseHttp.cs
@@ -42,7 +42,7 @@ namespace Films.Models.Web.HttpClients
 
         private void SetBaseHeaders()
         {
-            Client.DefaultRequestHeaders.Add("User-Agent", "AmPirate");
+            Client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0");
             Client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
             Client.DefaultRequestHeaders.Add("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
         }
