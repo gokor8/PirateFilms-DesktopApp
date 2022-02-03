5fa5185d3085e5eb10ea769b27f301aef44f337a
Grigoriy Paravyan <gvalal@bk.ru>
2022-02-03 14:20:50 +0300

    Измененная анимация для TextBlock фильмов

--- Films/Views/MainWindow.xaml.cs
+++ Films/Views/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace Films
 {
@@ -13,6 +18,17 @@ namespace Films
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void FilmName_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            /*var textBlock = sender as TextBlock; 
+            var doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0;
+            doubleAnimation.To = textBlock.ActualWidth;
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            textBlock.BeginAnimation(textBlock.ActualWidth, doubleAnimation);*/
         }
     }
 }
