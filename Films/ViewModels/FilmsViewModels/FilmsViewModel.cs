cef682c5647e2ccebf388341cbf73e68b274234e
Grigoriy Paravyan <gvalal@bk.ru>
2022-02-04 23:39:37 +0300

    Создаю подгружателя актуальных(популярных) фильмов

---
+++ Films/ViewModels/FilmsViewModels/FilmsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Windows.Media.Imaging;
+using Films.Models.Web.HttpClients;
+using Films.Models.Web.Parsers;
+using Films.ViewModels.MainViewModel;
+
+namespace Films.ViewModels.FilmsViewModels
+{
+    public class FilmsViewModel : INPC
+    {
+        private ObservableCollection<FilmDataViewModel> _popularFilms;
+        
+        public FilmsViewModel()
+        {
+            popularFilms = new ObservableCollection<FilmDataViewModel>() {new FilmDataViewModel(){Name = "...."}};
+        }
+        
+        public ObservableCollection<FilmDataViewModel> popularFilms
+        {
+            get =>_popularFilms;
+
+            set
+            {
+                _popularFilms = value;
+                
+                _ = Task.Run(async () =>
+                {
+                    var filmsSite = await SiteFilmsHttp.GetInstanceAsync();
+                    var siteHtml = await filmsSite.Client.GetStringAsync("/");
+                    var filmsCollection = await new FilmsPreviewLordfilmParser().GetPopularFilmsName(siteHtml, 5);
+
+                    ObservableCollection<FilmDataViewModel> filmsViewModels = new ObservableCollection<FilmDataViewModel>();
+
+                    foreach (var film in filmsCollection)
+                    {
+                        filmsViewModels.Add(new FilmDataViewModel()
+                        {
+                            Name = film.Name,
+                            PictureLink = film.Picture
+                        });
+                    }
+
+                    _popularFilms = filmsViewModels;
+                    
+                    OnPropertyChanged();
+                });
+            }
+        }
+    }
+}
---
+++ Films/ViewModels/MainViewModels/FilmViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Films.ViewModels.MainViewModels
+{
+    public class FilmViewModel : INPC
+    {
+        public string Name
+        {
+            get; set;
+        }
+
+        public string Picture
+        {
+            get; set;
+        }
+    }
+}
