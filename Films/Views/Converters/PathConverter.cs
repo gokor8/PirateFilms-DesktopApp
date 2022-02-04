2567121349f9199bf463706a1f7f52e7f23e297a
Grigoriy Paravyan <gvalal@bk.ru>
2022-02-04 23:58:42 +0300

    Несколько дизайнерских микро имзенений + добавляю валидацию пути

---
+++ Films/Views/Converters/PathConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Windows.Data;
+
+namespace Films.Views.Converters
+{
+    public class PathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string path = value.ToString();
+            if (path.StartsWith("\\"))
+            path = path.Substring(1);
+
+            return Path.Combine("whateveryourbasepathis", path);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
