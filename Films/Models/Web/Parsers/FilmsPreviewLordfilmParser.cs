a92d454a678b2c58a26222584c1dd1ca43c3e4c6
Grigoriy Paravyan <gvalal@bk.ru>
2022-02-04 23:40:51 +0300

    Стандартный парсер имен с сайта + его тест

---
+++ Films/Models/Web/Parsers/FilmsPreviewLordfilmParser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AngleSharp;
+using Films.MVVMLogic.Models;
+
+namespace Films.Models.Web.Parsers
+{
+    public class FilmsPreviewLordfilmParser : LordfilmParser<Film>
+    {
+        public override async Task<IEnumerable<Film>> GetPopularFilmsName(string siteHtml, int countFilms)
+        {
+            List<Film> filmNames = new List<Film>();
+            var htmlDocument = await _parser.Context.OpenAsync(req => req.Content(siteHtml));
+
+            var filmElements = htmlDocument.QuerySelectorAll("div.sect-cont.sect-items.clearfix div.th-item");
+
+            if (filmElements.Length <= 0)
+                return filmNames;
+
+
+            for (int countFilm = 0; countFilm < countFilms; countFilm++)
+            {
+                string nameFilm = _parser.ClearWhiteSpaces(filmElements[countFilm]?.QuerySelector("div.th-title")?.TextContent);
+                string pictureLink = filmElements[countFilm]?.QuerySelector("img")?.GetAttribute("srcset");
+
+                if (nameFilm == null || pictureLink == null)
+                    continue;
+                
+                filmNames.Add(new Film(nameFilm, "https://nu2.lordfilm3.cam" + pictureLink));
+            }
+
+            return filmNames;
+        }
+
+        public override async Task<IEnumerable<Film>> GetFilms()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ModelsTest/Models_Test/Web_Test/Parsers_Tests/FilmsPreviewLordfilmParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Films.Models.Web.BingSearch.SiteSearchers;
+using Films.Models.Web.HttpClients;
+using Films.Models.Web.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelsTest.Models_Test.Web_Test.Parsers_Tests
+{
+    [TestClass]
+    public class FilmsPreviewLordfilmParserTests
+    {
+        [TestMethod]
+        public async Task Get_One_bing_film_contains_site_name()
+        {
+            var localSiteSearcher = new LocalSiteSearcher();
+            await localSiteSearcher.SearchWorkingSitesAsync();
+
+            var workingLink = localSiteSearcher.WorkingLinks.FirstOrDefault();
+
+            if (workingLink == null)
+                Assert.Fail("В базе данных нет рабочей ссылки");
+
+            string siteHtml = await PublicHttp.GetInstance().Client.GetStringAsync(workingLink);
+
+            var filmsName = await new FilmsPreviewLordfilmParser().GetPopularFilmsName(siteHtml, 5);
+
+            Assert.IsTrue(filmsName.Count() == 5);
+        }
+    }
+}
