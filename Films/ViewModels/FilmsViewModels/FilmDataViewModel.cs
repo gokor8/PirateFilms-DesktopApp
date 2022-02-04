cd1dfb2e4012964316e3902eaa0851a45f4c6f06
Grigoriy Paravyan <gvalal@bk.ru>
2022-02-04 23:40:06 +0300

    ViewModel для отображения фильмов

---
+++ Films/ViewModels/FilmsViewModels/FilmDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace Films.ViewModels.FilmsViewModels
+{
+    public class FilmDataViewModel
+    {
+        public string Name { get; set; }
+        
+        public string PictureLink { get; set; }
+    }
+}
