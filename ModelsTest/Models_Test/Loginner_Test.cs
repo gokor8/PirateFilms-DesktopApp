212f45831d0b5b91fd050bb2ed4b22a9cee4a09c
Grigoriy Paravyan <gvalal@bk.ru>
2022-02-06 23:30:50 +0300

    Добавляю еще 1 проверку для более широкого теста

--- ModelsTest/Models_Test/Loginner_Test.cs
+++ ModelsTest/Models_Test/Loginner_Test.cs
@@ -6,29 +6,30 @@ namespace ModelsTest.MVVMLogic_Test.Models_Test
     [TestClass]
     public class Loginner_Test
     {
+        private LogginerBuilder _logginer = new LogginerBuilder();
         public Loginner_Test()
         {
             var instance = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
         }
-
-        private LoginnerBuilder _loginner = new LoginnerBuilder();
-
+        
         [TestMethod]
         public void GetVerifyResult_true_verify_login_password()
         {
-            var loginnerDataStore = _loginner.VerifyLogin("Krutoi").VerifyPassword("123456").GetVerifyResult();
+            var loginnerDataStore = _logginer.VerifyLogin("Krutoi").VerifyPassword("123456").GetVerifyResult();
 
             Assert.IsTrue(loginnerDataStore.IsVerify);
             Assert.AreEqual("", loginnerDataStore.AcscessString);
         }
 
         [TestMethod]
-        public void GetVerifyResult_false_verify_login_password()
+        [DataRow("NeKrutoi","336")]
+        [DataRow("Krutoi","336228")]
+        public void GetVerifyResult_false_verify_login_password(string login, string password)
         {
-            var loginnerDataStore = _loginner.VerifyLogin("NeKrutoi").VerifyPassword("336").GetVerifyResult();
+            var logginerDataStore = _logginer.VerifyLogin(login).VerifyPassword(password).GetVerifyResult();
 
-            Assert.IsFalse(loginnerDataStore.IsVerify);
-            Assert.AreEqual("Неверный логин; Неверный пароль; ", loginnerDataStore.AcscessString);
+            Assert.IsFalse(logginerDataStore.IsVerify);
+            Assert.IsTrue(logginerDataStore.AcscessString.Contains("Неверный"));
         }
     }
 }
